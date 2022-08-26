d452cf5bce6fed9c64e487c64dd2c2bed75dbcc9
Yasin051197 <yasinjamadar0511@gmail.com>
2022-08-26 09:58:33 +0530

    social shop page added in day-4

---
+++ day-4/social_shop/shop_social.js
@@ -0,0 +1,18 @@
+var wraper=document.getElementsByClassName("shop_the_feed_container")
+var btn=document.getElementById("loadmore")
+var currentchild=1;
+btn.addEventListener("click",function()
+{
+    for(let i=currentchild;i<currentchild+1;i++)
+    {
+        if(wraper[i])
+        {
+            wraper[i].style.display = 'block';
+        }
+    }
+    currentchild ++;
+    if(currentchild>=wraper.length)
+    {
+        event.target.style.display='none';
+    }
+})
